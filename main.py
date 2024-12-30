7c442046d9b009df4dc5c84d950dcb25ff7da7c4
BossAHPGaming <aaravhpatel210@gmail.com>
2024-12-30 18:32:28 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import turtle 
+
+turtle.Screen().bgcolor("blue")
+
+sc = turtle.Screen()
+sc.setup(400,300)
+
+turtle.title("Welcome to Turtle Windows")
+
+board = turtle.Turtle()
+
+board.forward(100)
+board.left(90)
+board.forward(120)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+board.forward(120)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+board.forward(120)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+board.forward(120)
+board.left(90)
